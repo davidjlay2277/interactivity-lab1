1912d71e84ba3c8b11a8dd092563d8b550421eee
David Lay <davidjlay2277@gmail.com>
2023-06-25 20:11:28 -0500

    delete button working

--- server/index2.js
+++ server/index2.js
@@ -1,4 +1,4 @@
-//////////////////COPY index1.js
+//////////////////COPY index1.js /////////////////////////////////////////
 ///////////////////put all the pieces together in a function//////////////
 
 console.log("hello again, world");
@@ -9,10 +9,27 @@ function addMovie(event) {
   const movieList = document.querySelector("#list");
   const movie = document.createElement("li");
   const movieTitle = document.createElement("span");
+ 
   movieList.appendChild(movie);
   movie.appendChild(movieTitle);
-  //I had .textContent instead of .value
+
+  const deleteBtn = document.createElement("button");
+  movie.appendChild(deleteBtn)
+  deleteBtn.textContent = "x";
+//previoulsy outside the addMovie function
+deleteBtn.addEventListener("click",deleteMovie);
+  //previously had .textContent instead of .value
   movieTitle.textContent = inputField.value;
+ inputField.value = "";
 }
+// /what would be the functional difference of pointing the query at the form instead of the button? what if the form had multiple buttons? 
 const event1 = document.querySelector("#button1");
 event1.addEventListener("click", addMovie);
+
+function deleteMovie(event) {
+  alert("movie entry was deleted")
+event.target.parentNode.remove()
+}
+
+document.querySelector(deleteBtn).addEventListener("click",deleteMovie);
+
